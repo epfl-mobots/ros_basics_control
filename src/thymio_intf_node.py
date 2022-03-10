f2ddfc660d61edd2e2c91dfef9267d4b7f7082cf
Vaios Papaspyros <b.papaspyros@gmail.com>
2022-03-10 20:22:50 +0100

    Add aukey webcam in launch files + correct thymio diff drive

--- src/thymio_intf_node.py
+++ src/thymio_intf_node.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-from thymiodirect import Connection
-from thymiodirect import Thymio
+from tdmclient import ClientAsync
 
 import rospy
 from ros_basics_msgs.msg import SimpleVelocities
@@ -14,12 +13,9 @@ from geometry_msgs.msg import Twist
 
 class ThymioInterface:
     def __init__(self):
-        self._port = Connection.serial_default_port()
-        self._th = Thymio(
-            serial_port=self._port,
-            on_connect=lambda node_id: print(f"{node_id} is connected"))
-        self._th.connect()
-        self._id = self._th.first_node()
+        self._client = ClientAsync()
+        self._node = self._client.aw(self._client.wait_for_node())
+        self._client.aw(self._node.lock_node())
 
         rospy.init_node('thymio_intf_node', anonymous=True)
         self._psensor_pub = rospy.Publisher(
@@ -30,16 +26,20 @@ class ThymioInterface:
 
         self._lb_new = -500.
         self._ub_new = 500.
-        self._lb_old = -0.2
-        self._ub_old = 0.2
+        self._lb_old = -0.14
+        self._ub_old = 0.14
         self._wheel_rad = 0.022
         self._wheel_dist_2 = 0.0475
-        self._regu = 45  # TODO: check this
+        # self._regu = 45  # TODO: check this
+        self._rate = 10
 
     def spin(self):
-        loop_rate = rospy.Rate(10)
+        loop_rate = rospy.Rate(self._rate)
         while not rospy.is_shutdown():
-            self._publish_psensors(self._th[self._id]["prox.horizontal"])
+            self._client.aw(self._node.wait_for_variables({"prox.horizontal"}))
+            prox = list(self._node.v.prox.horizontal)
+            self._node.flush()
+            self._publish_psensors(prox)
             loop_rate.sleep()
 
     def _publish_psensors(self, values):
@@ -49,12 +49,10 @@ class ThymioInterface:
         self._psensor_pub.publish(ps)
 
     def _vel_callback(self, data):
-        l_speed = (data.v + self._wheel_dist_2 * -data.w) / \
-            self._wheel_rad
-        r_speed = (data.v - self._wheel_dist_2 * -data.w) / \
-            self._wheel_rad
-        l_speed /= self._regu
-        r_speed /= self._regu
+        l_speed = (2 * data.v + self._wheel_dist_2 * -data.w) / 2
+        r_speed = (2 * data.v - self._wheel_dist_2 * -data.w) / 2
+        # l_speed /= self._regu
+        # r_speed /= self._regu
         l_speed = self._rescale(l_speed)
         r_speed = self._rescale(r_speed)
         self._move(l_speed, r_speed)
@@ -63,8 +61,12 @@ class ThymioInterface:
         # print("(l, r): ({}, {})".format(l_speed, r_speed))
         l_speed = l_speed if l_speed >= 0 else 2 ** 16 + l_speed
         r_speed = r_speed if r_speed >= 0 else 2 ** 16 + r_speed
-        self._th[self._id]["motor.left.target"] = l_speed
-        self._th[self._id]["motor.right.target"] = r_speed
+        cmd = {
+            "motor.left.target": [l_speed],
+            "motor.right.target": [r_speed],
+        }
+        self._node.send_set_variables(cmd)
+        self._node.flush()
 
     def _rescale(self, val):
         if val > self._ub_old:
